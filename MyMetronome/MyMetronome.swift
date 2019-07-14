aada7b50c8b501fdda8d642e8394e60a279d9c1e
Mike Vartanian <mike.vartanian@gmail.com>
2019-07-14 01:14:23 -0400

    Initial add of metronome code.

---
+++ MyMetronome/MyMetronome.swift
@@ -0,0 +1,66 @@
+//
+//  MyMetronome.swift
+//  MyMetronome
+//
+//  Created by Michael Vartanian on 7/5/19.
+//  Copyright © 2019 Michael Vartanian. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class Metronome {
+    var bpm: Float = 60.0 { didSet {
+        bpm = min(300.0,max(30.0,bpm))
+        }}
+    var enabled: Bool = false { didSet {
+        if enabled {
+            start()
+        } else {
+            stop()
+        }
+        }}
+    var onTick: ((_ nextTick: DispatchTime) -> Void)?
+    var nextTick: DispatchTime = DispatchTime.distantFuture
+    
+    let player: AVAudioPlayer = {
+        do {
+            let soundURL = Bundle.main.url(forResource: "Woodblock", withExtension: "wav")!
+            let soundFile = try AVAudioFile(forReading: soundURL)
+            let player = try AVAudioPlayer(contentsOf: soundURL)
+            return player
+        } catch {
+            print("Oops, unable to initialize metronome audio buffer: \(error)")
+            return AVAudioPlayer()
+        }
+    }()
+    
+    private func start() {
+        print("Starting metronome, BPM: \(bpm)")
+        nextTick = DispatchTime.now()
+        player.prepareToPlay()
+        nextTick = DispatchTime.now()
+        tick()
+    }
+    
+    private func stop() {
+        player.stop()
+        print("Stoping metronome")
+    }
+    
+    private func tick() {
+        guard
+            enabled,
+            nextTick <= DispatchTime.now()
+            else { return }
+        
+        let interval: TimeInterval = 60.0 / TimeInterval(bpm)
+        nextTick = nextTick + interval
+        DispatchQueue.main.asyncAfter(deadline: nextTick) { [weak self] in
+            self?.tick()
+        }
+        
+        player.play(atTime: interval)
+        onTick?(nextTick)
+    }
+}
---
+++ MyMetronome/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyMetronome
+//
+//  Created by Michael Vartanian on 7/5/19.
+//  Copyright © 2019 Michael Vartanian. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
