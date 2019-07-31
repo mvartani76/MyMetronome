0e6c23fda766b27e5883da00b271bb32ea8e288b
Mike Vartanian <mike.vartanian@gmail.com>
2019-07-31 13:27:15 -0400

    Initial testing to hear metronome play with simple tick. This appears to work.

--- MyMetronome/FirstViewController.swift
+++ MyMetronome/FirstViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var startStopButton: UIButton!
+    @IBOutlet var tickLabel: UILabel!
+    
+    var isToggled = false
+    
+    let myMetronome = Metronome()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        myMetronome.onTick = { (nextTick) in
+            self.animateTick()
+        }
+        updateBpm()
     }
-
+    
+    private func animateTick() {
+        tickLabel.alpha = 1.0
+        UIView.animate(withDuration: 0.35) {
+            self.tickLabel.alpha = 0.0
+        }
+    }
+    
+    
+    @IBAction func toggleMetronomeButton(_ sender: UIButton) {
+        if !isToggled {
+            myMetronome.enabled = true
+            isToggled = true
+        } else {
+            myMetronome.enabled = false
+            isToggled = false
+        }
+    }
+    
+    private func updateBpm() {
+        let metronomeBpm = Int(myMetronome.bpm)
+        //bpmLabel.text = "\(metronomeBpm)"
+    }
+    
 
 }
 
