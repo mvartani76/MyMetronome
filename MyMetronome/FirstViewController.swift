99405d051025230e42c030d5d4dc83fa40f78c23
Mike Vartanian <mike.vartanian@gmail.com>
2019-08-06 10:22:45 -0400

    Initial add of pickerviews for time signature. Changing the time signature updates the meterview. Still need to add constants file and work out sizing of circles in meter view when less than 4 beats per measure.

--- MyMetronome/FirstViewController.swift
+++ MyMetronome/FirstViewController.swift
@@ -8,7 +8,47 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    // Input the data into the array
+    var numBeatsData = ["1", "2", "3", "4", "5", "6"]
+    var beatNoteData = ["1", "2", "3", "4", "5", "6"]
+    
+    var numBeats: Int = 4
+    var beatNote: Int = 4
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        if pickerView == numBeatsPickerView {
+            return numBeatsData.count
+        }
+        else if pickerView == beatNotePickerView {
+            return beatNoteData.count
+        }
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == numBeatsPickerView {
+            return numBeatsData[row]
+        } else {
+            return beatNoteData[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == numBeatsPickerView {
+            numBeats = Int(numBeatsData[row]) ?? 4
+        } else if pickerView == beatNotePickerView {
+            beatNote = Int(beatNoteData[row]) ?? 4
+        }
+        updateTimeSignature(numBeats: numBeats, beatNote: beatNote)
+    }
+    
     @IBOutlet var knob: Knob3!
     @IBOutlet var myMeterView: MeterView!
     
@@ -17,10 +57,30 @@ class FirstViewController: UIViewController {
     @IBOutlet var bpmLabel: UILabel!
     @IBOutlet var incBPMButton: UIButton!
     @IBOutlet var decBPMButton: UIButton!
+    @IBOutlet var timeSignatureLabel: UILabel!
+    @IBOutlet var numBeatsPickerView: UIPickerView!
+    @IBOutlet var beatNotePickerView: UIPickerView!
+    
     
     var isToggled = false
     
     let myMetronome = Metronome()
+    
+    
+    var pickerData: [String] = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Connect data:
+        self.numBeatsPickerView.delegate = self
+        self.numBeatsPickerView.dataSource = self
+        self.beatNotePickerView.delegate = self
+        self.beatNotePickerView.dataSource = self
+        
+        myMeterView.numBeats = 4
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -29,20 +89,17 @@ class FirstViewController: UIViewController {
             self.animateTick()
         }
         updateBpm(newBPM: Float(knob.value))
-        myMeterView.numBeats = 4
+        updateTimeSignature(numBeats: 4, beatNote: 4)
+        
         // Initialize the currentBeat to 0 to have the first tick highlight the leftmost circle
         myMeterView.currentBeat = 0
+        
+        numBeatsPickerView.selectRow(3, inComponent: 0, animated: true)
+        beatNotePickerView.selectRow(3, inComponent: 0, animated: true)
     }
     
     private func animateTick() {
-        tickLabel.alpha = 1.0
-        UIView.animate(withDuration: 0.35) {
-            self.tickLabel.alpha = 0.0
-        }
-
-        print(myMeterView.currentBeat)
         myMeterView.setNeedsDisplay()
-        
         myMeterView.updateMeter()
     }
     
@@ -73,13 +130,16 @@ class FirstViewController: UIViewController {
         updateBpm(newBPM: myMetronome.bpm - 1.0)
     }
     
-    
+
     private func updateBpm(newBPM: Float) {
         myMetronome.bpm = min(max(30, newBPM), 300)
         bpmLabel.text = String(format: "%.0f", myMetronome.bpm)
         knob.value = myMetronome.bpm
     }
     
-
+    private func updateTimeSignature(numBeats: Int, beatNote: Int) {
+        timeSignatureLabel.text = "\(numBeats)/\(beatNote)"
+        myMeterView.numBeats = numBeats
+        myMeterView.setNeedsDisplay()
+    }
 }
-
