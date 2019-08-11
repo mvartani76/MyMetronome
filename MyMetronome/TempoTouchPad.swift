2e35763b0fe357fe1dc4bf9d28017703880ff9b0
Mike Vartanian <mike.vartanian@gmail.com>
2019-08-11 17:14:36 -0400

    Removed the code used to create touch effect where the user touched the tapTouchPad.

--- MyMetronome/TempoTouchPad.swift
+++ MyMetronome/TempoTouchPad.swift
@@ -17,7 +17,6 @@ class TempoTouchPad: UIControl, CAAnimationDelegate {
     var tapDiff = .numSecondsInMinute / .startingTempo
     
     let animation = CABasicAnimation()
-    var shapeLayer = CAShapeLayer()
     
     
     /*
@@ -40,49 +39,11 @@ class TempoTouchPad: UIControl, CAAnimationDelegate {
         
         // Filter the tap BPM so it is not super sensitive on output
         TempoTouchPad.tapBPM = (1.0 - .tapBPMFiltCoeff) * TempoTouchPad.tapBPM + .tapBPMFiltCoeff * curBPM
-        
-        /*
-        if let touch = touches.first {
-            // Set the Center of the Circle
-            // 1
-            let circleCenter = touch.location(in: self)
-            let circleRadius = CGFloat(15)
-                
-            // Create a new CircleView
-            // 3
-            let circle = UIBezierPath(arcCenter: circleCenter,
-            radius: circleRadius,
-            startAngle: 0,
-            endAngle: CGFloat(2 * Double.pi),
-            clockwise: true)
-            
-            shapeLayer = CAShapeLayer()
-            shapeLayer.path = circle.cgPath
-            shapeLayer.fillColor = UIColor.blue.cgColor
-            shapeLayer.strokeColor = UIColor.blue.cgColor
-            shapeLayer.lineWidth = 1
-            
-            shapeLayer.shadowColor = UIColor.black.cgColor
-            //shapeLayer.shadowOffset = CGSize(width: 0, height: 0)
-            shapeLayer.shadowRadius = CGFloat(50)
-            shapeLayer.shadowOpacity = 1.0
-            
-            
-            
-            // Create a blank animation using the keyPath "cornerRadius", the property we want to animate
-            animateProperty(property: "opacity", fromValue: 0.0, toValue: 0.5, duration: 0.25)
-            animateProperty(property: "borderWidth", fromValue: 5.0, toValue: 50.0, duration: 0.25)
-            
-            layer.addSublayer(shapeLayer)
-        }
-        */
     }
     
     func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
        
         print("animaton stopped")
-        
-        //shapeLayer.removeFromSuperlayer()
     }
     
     // Method to animate property
